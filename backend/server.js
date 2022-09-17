5219b3f10132f94cfe1e61d763335b9cd206e0e7
ZinoTrust <zinotrust@gmail.com>
2022-09-17 12:46:03 +0100

    starter-project

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+// Connect to DB and start server
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server Running on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
