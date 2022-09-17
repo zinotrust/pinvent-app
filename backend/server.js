87e7f555ecc6f98cebeb3d6fcb4bf285cc727eb3
ZinoTrust <zinotrust@gmail.com>
2022-09-17 12:55:21 +0100

    Setup server and connect MobgoDB

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,18 @@ const cors = require("cors");
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
 
 // Connect to DB and start server
+const PORT = process.env.PORT || 5000;
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
