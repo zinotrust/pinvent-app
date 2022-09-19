eb8db3e875b29b60e9c249feeef75a58b3691b4d
ZinoTrust <zinotrust@gmail.com>
2022-09-19 04:46:23 +0100

    next

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -53,6 +53,7 @@ userSchema.pre("save", async function (next) {
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(this.password, salt);
   this.password = hashedPassword;
+  next();
 });
 
 const User = mongoose.model("User", userSchema);
