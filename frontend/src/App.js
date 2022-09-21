36e05f450489962d92a3b5dace4249d619a28474
ZinoTrust <zinotrust@gmail.com>
2022-09-21 10:48:02 +0100

    Routing Setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+
 function App() {
   return (
-    <div>
-      <h1>Hello World!!</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
