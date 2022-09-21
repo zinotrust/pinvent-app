a66d144358e58348ecc87ac4aa15ff60a1fe4614
ZinoTrust <zinotrust@gmail.com>
2022-09-21 08:25:36 +0100

    11. Create React App and setup Redux

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello World!!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/redux/features/auth/authSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  isLoggedIn: false,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {},
+});
+
+export const {} = authSlice.actions;
+
+export default authSlice.reducer;
