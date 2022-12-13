5a8de515816eb37f46861c286346871ff336b3da
Sodiq Baki Adeiza <58841467+NaijaTechBro@users.noreply.github.com>
2022-12-13 10:40:52 +0100

    Update userModel.js

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -14,7 +14,7 @@ const userSchema = mongoose.Schema(
       trim: true,
       match: [
         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-        "Please enter a valid emaial",
+        "Please enter a valid email",
       ],
     },
     password: {
